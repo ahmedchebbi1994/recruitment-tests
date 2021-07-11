7b43577724f431957d61538327dbe214584c0ce1
Ahmed Chebbi <ahmed.chebbi.ext@orange.com>
2021-07-11 19:49:47 +0200

    implement network layer add models (Offer books)

---
+++ recruitment-tests-ahmed-chebbi/recruitment-tests-ahmed-chebbi/Networks/Models/Offer.swift
@@ -0,0 +1,12 @@
+//
+//  Offer.swift
+//  recruitment-tests-ahmed-chebbi
+//
+//  Created by Chebbi on 11/07/2021.
+//
+
+struct Offer: Codable {
+    let type: String
+    let value: Int
+    let sliceValue: Int?
+}
